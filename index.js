02062271103cc08ac6e6cce74b35bd623a8e71cd
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2021-11-14 02:20:54 +0600

    start and get , delete product functionality add

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const app = express();
+const { MongoClient } = require("mongodb");
+const cors = require('cors');
+const objectId = require('mongodb').ObjectId;
+require('dotenv').config();
+
+
+//Middle were ------------------------------------------
+app.use(cors());
+app.use(express.json());
+
+//Port---------------------------------------------------
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ne473.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        // Connect the client to the server
+        await client.connect();
+
+        const database = client.db("gorgeous_watch_shop");
+        const watchesCollection = database.collection("watches");
+        const parchesCollection = database.collection("parches");
+
+        // get all watches info -------------------------------------
+        app.get('/watches', async (req, res) => {
+            const data = watchesCollection.find({});
+            const watchesData = await data.toArray();
+            res.json(watchesData);
+        });
+
+        // post parches data ----------------------------------------
+        app.post('/parches', async (req, res) => {
+            const parchesData = req.body;
+            const result = await parchesCollection.insertOne(parchesData);
+            res.json(result);
+        });
+
+        // get parches data -----------------------------------------
+        app.get('/parches', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const parchesData = parchesCollection.find(query);
+            const result = await parchesData.toArray();
+            res.send(result);
+        });
+
+        // delete my order watch products --------------------------
+        app.delete('/parches/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: objectId(id) };
+            const result = await parchesCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+
+        console.log('data base connected');
+
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+//test get -----------------------------------------------
+app.get('/', (req, res) => {
+    res.send('connected');
+})
+// listening port -----------------------------------------
+app.listen(port, () => {
+    console.log('listening port', port);
+});
